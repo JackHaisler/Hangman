89c676a6092134f65fa277417d3b42a82964c568
taylun-surles <jane@williams.org>
2022-04-28 14:44:54 -0500

    here

--- Sources/CursesShell/Game.swift
+++ Sources/CursesShell/Game.swift
@@ -15,11 +15,24 @@ class Game {
 
         let dictionaryURL = URL.init(fileURLWithPath: "/usr/share/dict/words")
         let contents = try String(contentsOf: dictionaryURL, encoding: .utf8)
-        let lines = contents.split(separator: "\n")
-        mainWindow.write("loaded \(lines.count) words")
+        let words = contents.split(separator: "\n")
+        mainWindow.write("loaded \(words.count) words")
         mainWindow.refresh()
 
         // Dictionary is now loaded.  Filter words to include only those suitable.
+        // We eliminate those words that have an apostrophe and also ensure that all words are lowercased
+        var goodWords = [String]()
+        for word in words {
+            if !word.contains("'") {
+                goodWords.append(word.lowercased())
+            }
+        }
+
+        // Select a random word
+        let randomWord = goodWords.randomElement()!
+        mainWindow.write(" The randomly selected word is \(randomWord)")
+        mainWindow.refresh()
+        
         
         
         
