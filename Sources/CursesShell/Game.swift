0bdc607c744ab8c23e87f620fd925a5eb47ba7c8
taylun-surles <jane@williams.org>
2022-05-04 14:42:19 -0500

    game

--- Sources/CursesShell/Game.swift
+++ Sources/CursesShell/Game.swift
@@ -16,7 +16,7 @@ class Game {
         let dictionaryURL = URL.init(fileURLWithPath: "/usr/share/dict/words")
         let contents = try String(contentsOf: dictionaryURL, encoding: .utf8)
         let words = contents.split(separator: "\n")
-        mainWindow.write("loaded \(words.count) words")
+        mainWindow.write("loaded \(words.count) words...")
         mainWindow.refresh()
 
         // Dictionary is now loaded.  Filter words to include only those suitable.
@@ -29,21 +29,30 @@ class Game {
         }
 
         // Select a random word
-        let randomWord = goodWords.randomElement()!
-        mainWindow.write(" The randomly selected word is \(randomWord)")
+        mainWindow.write("selecting random word...")
         mainWindow.refresh()
         
+        let randomWord = goodWords.randomElement()!
+        let wordDisplay = WordDisplay(word: randomWord)
         
-        
+        mainWindow.write("word selected.  Ready to play!")
+        mainWindow.refresh()
         
         var shouldQuit = false
         
         repeat {
-            mainWindow.write("Welcome to Hangman. Would you like to start a game? Input y/n")
-            mainWindow.getStringFromTextField(at: Point(x: 10, y: 10),
-                                              maxCharacters: 1,
-                                              fieldColorPair: nil)
+            mainWindow.cursor.position = Point(x: 0, y: 5)
+            mainWindow.write("Welcome to Hangman. Enter your guess:")
+            let guess = mainWindow.getStringFromTextField(at: Point(x: 38, y: 5),
+                                                          maxCharacters: 1,
+                                                          fieldColorPair: nil)
+
             
+            mainWindow.write("You guessed: \(guess)")
+            mainWindow.refresh()
+            
+            wordDisplay.writeToWindow(window: mainWindow, at: Point(x: 50, y: 5 ))
+            mainWindow.refresh()
 
         } while !shouldQuit
     }
